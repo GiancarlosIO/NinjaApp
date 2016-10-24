cfbf66eb6a8080f6e9a670a201f027fadec82bdd
giancarlosio <giancarlos.isasi@gmail.com>
2016-10-23 20:38:31 -0500

    Config route-provider for views

--- app/assets/javascripts/ninja/app.js
+++ app/assets/javascripts/ninja/app.js
@@ -2,5 +2,20 @@
     "use strict";
     angular
         .module('ninjaApp', ['ngRoute', 'templates'])
-        .config();
+        .config(configApp);
+
+    configApp.$inject = ['$routeProvider'];
+
+    function configApp($routeProvider) {
+      $routeProvider
+        .when('/home', {
+          templateUrl: 'home_template.html'
+        })
+        .when('/directory', {
+          templateUrl: 'directory_template.html',
+          controller: 'NinjasController as ninjas'
+        }).otherwise({
+          redirectTo: '/home'
+        })
+    }
 })();
